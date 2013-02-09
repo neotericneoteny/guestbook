f8fb59ded17286895c49f7dd74c284c3aecbd47a
Matthew Zimmer <matt@neotericneoteny.com>
2013-02-08 23:10:03 -0500

    Completed "Using the Users Service" at https://developers.google.com/appengine/docs/java/gettingstarted/usingusers

--- src/guestbook/GuestbookServlet.java
+++ src/guestbook/GuestbookServlet.java
@@ -1,5 +1,9 @@
 package guestbook;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +20,14 @@ import java.io.IOException;
 public class GuestbookServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+
+		if (user != null) {
+			resp.setContentType("text/plain");
+			resp.getWriter().println("Hello, " + user.getNickname());
+		} else {
+			resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
+		}
 	}
 }
